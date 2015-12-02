40ed5c4a481aafc976622e67543af392eee67fd5
Michael Angerman <stormasm@gmail.com>
2015-12-01 17:10:18 -0800

    ex02

--- ex02.js
+++ ex02.js
@@ -9,7 +9,26 @@ var copyFile = function(source, destination, next) {
   });
 };
 
+var copyFile1 = function(source, destination, next) {
+  // we should read source file first
+  fs.readFile(source, function(err, data) {
+    if (err) return next(new Error('I cannot do it!')); // error
+    // now we can write data to destination file
+    fs.writeFile(destination, data, next);
+    next(null, destination); // no error occurred, return result
+  });
+};
+
 copyFile('foo.txt', 'bar.txt', function(err) {
+  if (err) {
+    // either fs.readFile or fs.writeFile returned an error
+    console.log(err.stack || err);
+  } else {
+    console.log('Success!');
+  }
+});
+
+copyFile1('foo.txt', 'bar.txt', function(err) {
   if (err) {
     // either fs.readFile or fs.writeFile returned an error
     console.log(err.stack || err);
