3cfd0ee4e7afb68e09ff3e77d7b9fb5c599f63ac
Michael Angerman <stormasm@gmail.com>
2015-12-09 12:21:16 -0800

    update ex00

--- ex00.js
+++ ex00.js
@@ -7,3 +7,17 @@ var usingItNow = function(callback) {
 };
 
 usingItNow(myCallback);
+
+// http://stackoverflow.com/questions/13286233/pass-a-javascript-function-as-parameter
+
+function addContact(id, refreshCallback) {
+    refreshCallback();
+    // You can also pass arguments if you need to
+    // refreshCallback(id);
+}
+
+function refreshContactList() {
+    console.log('Hello World');
+}
+
+addContact(1, refreshContactList);
