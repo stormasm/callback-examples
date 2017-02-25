569ee9f02217d77fa8d4b8038a1c3ecffbbdee63
Michael Angerman <stormasm@gmail.com>
2017-02-24 17:47:33 -0800

    promise02

--- promise02.js
+++ promise02.js
@@ -3,24 +3,31 @@ var promise = new Promise(function(resolve, reject) {
 });
 
 promise.then(function(val) {
-        console.log(val); // 1
+        console.log('a', val); // 1
         return val + 2;
     },
     function(err) {
-        console.log("It broke", err); // Error: "It broke"
+        console.log('a broke', err); // Error: "It broke"
     }
 ).then(function(val) {
-        console.log(val); // 3
+        console.log('b', val); // 3
         return val + 3;
     },
     function(err) {
-        console.log(err)
+        console.log('b broke', err)
     }
 ).then(function(val) {
-        console.log(val);
+        console.log('c', val);
         return val + 5;
     },
     function(err) {
-        console.log(err)
+        console.log('c broke', err)
+    }
+).then(function(val) {
+        console.log('d', val);
+        return val + 10;
+    },
+    function(err) {
+        console.log('d broke', err)
     }
 )
